7433b918ef12d63722f54be52d71bf1f4e56cfc2
engjoao <aquinosilva@gmail.com>
2016-09-27 10:44:40 -0300

    Mark as complete ability for tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,6 @@
 class TasksController < ApplicationController
 	before_action :set_project
+	before_action :set_task, except: [:create]
 
 	def create
 		@task = @project.tasks.create(task_params)
@@ -7,7 +8,6 @@ class TasksController < ApplicationController
 	end
 
 	def destroy
-		@task = @project.tasks.find(params[:id])
 		if @task.destroy
 			flash[:sucess] = "Task was deleted."
 		else
@@ -17,10 +17,19 @@ class TasksController < ApplicationController
 
 	end
 
+	def complete
+		@task.update_attribute(:completed_at, Time.now)
+		redirect_to @project, notice: "Task complete"
+	end
+
 	private
 
 	def set_project
 		@project = Project.find(params[:project_id])
+	end
+
+	def set_task
+		@task = @project.tasks.find(params[:id])
 	end
 
 	def task_params
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160926184128) do
+ActiveRecord::Schema.define(version: 20160926204100) do
 
   create_table "projects", force: true do |t|
     t.string   "title"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20160926184128) do
     t.integer  "project_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.datetime "completed_at"
   end
 
   add_index "tasks", ["project_id"], name: "index_tasks_on_project_id"
